f5302f5d53fe05170a724ef9fe04897291021bff
root <juneho.bang85@gmail.com>
2022-10-18 15:57:18 +0900

    add and delete a vlan in a PICA switch

--- vlancreation/vlancreation.py
+++ vlancreation/vlancreation.py
@@ -3,16 +3,41 @@
 
 from ncclient import manager
 
-snippet = """
-<vlans xmlns="http://pica8.com/xorplus/vlans">
-   <vlan-id>
-      <id>136</id>
-      <vlan-name>testvlan</vlan-name>
-      <l3-interface>vlan136</l3-interface>
-   </vlan-id>
-</vlans>
+# snippet = """
+# <vlans xmlns="http://pica8.com/xorplus/vlans">
+#    <vlan-id>
+#       <id>136</id>
+#       <vlan-name>testvlan</vlan-name>
+#       <l3-interface>vlan136</l3-interface>
+#    </vlan-id>
+# </vlans>
+# """
+eos = manager.connect(host="172.31.32.102", port="830",  timeout=500, username="admin", password="password",  hostkey_verify=False, device_params={'name':'default'})
+
+add_snippet = """
+<config>
+   <vlans xmlns="http://pica8.com/xorplus/vlans">
+      <vlan-id>
+         <id>136</id>
+         <vlan-name>testvlan</vlan-name>
+         <l3-interface>vlan136</l3-interface>
+      </vlan-id>
+   </vlans>
+</config>
 """
 
-with manager.connect(host="172.31.32.102", port="830",  timeout=30, username="admin", password="password",  hostkey_verify=False, device_params={'name':'default'}) as m:
-   reply = m.edit_config(target="running", config=snippet, default_operation="merge")
-   print(reply.xml)
+reply = eos.edit_config(target="running", config=add_snippet, default_operation="merge")
+print(reply.xml)
+
+
+delete_snippet = """
+<config>
+   <vlans xmlns="http://pica8.com/xorplus/vlans">
+      <vlan-id operation="delete">
+         <id>136</id>
+      </vlan-id>
+   </vlans>
+</config>
+"""
+
+reply = eos.edit_config(target="running", config=delete_snippet, default_operation="merge")
