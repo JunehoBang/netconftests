31d174c45e5956add2a4dae25a5d87cad78bc142
root <juneho.bang85@gmail.com>
2022-10-27 11:39:28 +0900

    static route add/del

--- staticrouting/staticrouting.py
+++ staticrouting/staticrouting.py
@@ -1,6 +1,6 @@
 from ncclient import manager
 
-eos = manager.connect(host="172.31.32.102", port="830",  timeout=500, username="admin", password="password",  hostkey_verify=False, device_params={'name':'default'})
+eos = manager.connect(host="172.31.32.200", port="830",  timeout=500, username="admin", password="password",  hostkey_verify=False, device_params={'name':'default'})
 
 add_snippet="""
 <config>
@@ -23,3 +23,16 @@ get_snippet="""
 """
 
 eos.get (filter=("subtree", get_snippet))
+
+
+del_snippet="""
+<config>
+    <static xmlns="http://pica8.com/xorplus/static-routes">
+        <route operation="delete">
+            <name>10.0.0.0/16</name>
+        </route>
+    </static>
+</config>
+"""
+
+eos.get (filter=("subtree", del_snippet))
