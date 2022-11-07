ba8ceb47d455e41eb769ba8c815343addd51bfe8
root <juneho.bang85@gmail.com>
2022-11-07 17:26:43 +0900

    VxLAN config add

---
+++ vxlan/vxlan.py
@@ -0,0 +1,17 @@
+from ncclient import manager
+
+eos = manager.connect(host="172.31.32.102", port="830",  timeout=500, username="admin", password="password",  hostkey_verify=False, device_params={'name':'default'})
+
+add_snippet = """
+<config>
+   <vxlans xmlns="http://pica8.com/xorplus/vxlans">
+        <source-interface>
+            <name>loopback</name>
+            <address>5.5.5.5</address>
+        </source-interface>
+        <udp-port>6000</udp-port>
+   </vxlans>
+</config>
+"""
+
+eos.edit_config(target="running", config=add_snippet, default_operation="merge")
